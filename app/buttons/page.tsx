c28a99892ac8d4a442e9da74ba3453d6c5f3267f
Arjun Singh <er.arjun126@gmail.com>
2024-03-22 13:06:00 +0530

    update

---
+++ app/buttons/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+export default function buttonPage() {
+  return (
+    <div>
+      <Button>Primary</Button>
+    </div>
+  );
+}
